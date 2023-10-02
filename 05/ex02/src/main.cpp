1ae3e70bc8fd8da092885bc0493898fc9edc4005
Ssuamje <side2850@naver.com>
2023-10-02 17:03:53 +0900

    main completed

--- 05/ex02/src/main.cpp
+++ 05/ex02/src/main.cpp
@@ -1,8 +1,62 @@
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main() {
+    Bureaucrat sanan("sanan", 1);
+    Bureaucrat babo("babo", 150);
+    ShrubberyCreationForm scFormYooh("yooh");
 
-    ShrubberyCreationForm form("home");
-    form.execute(Bureaucrat("Bureaucrat", 1));
+    std::cout << std::endl;
+
+    std::cout << sanan << std::endl;
+    std::cout << babo << std::endl;
+    std::cout << scFormYooh << std::endl;
+
+    std::cout << std::endl;
+
+    babo.signForm(scFormYooh);
+    sanan.signForm(scFormYooh);
+    babo.executeForm(scFormYooh);
+    sanan.executeForm(scFormYooh);
+
+    std::cout << std::endl;
+    std::cout << "*******************************************" << std::endl;
+
+    RobotomyRequestForm rrFormJpark("jpark2");
+    
+    std::cout << std::endl;
+
+    std::cout << rrFormJpark << std::endl;
+    
+    std::cout << std::endl;
+    
+    babo.signForm(rrFormJpark);
+    sanan.signForm(rrFormJpark);
+    babo.executeForm(rrFormJpark);
+    sanan.executeForm(rrFormJpark);
+    sanan.executeForm(rrFormJpark);
+    sanan.executeForm(rrFormJpark);
+    sanan.executeForm(rrFormJpark);
+
+    std::cout << std::endl;
+    std::cout << "*******************************************" << std::endl;
+
+    PresidentialPardonForm ppFormJbok("jbok");
+
+    std::cout << std::endl;
+
+    std::cout << ppFormJbok << std::endl;
+
+    std::cout << std::endl;
+    
+    babo.signForm(ppFormJbok);
+    sanan.signForm(ppFormJbok);
+    babo.executeForm(ppFormJbok);
+    sanan.executeForm(ppFormJbok);
+
+    std::cout << std::endl;
+    std::cout << "********************END********************" << std::endl;
+
     return 0;
 }
